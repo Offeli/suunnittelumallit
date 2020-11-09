2ee19e50e4326dfcfc74e6d2a6311a195fa9c0f2
Offeli <49982204+Offeli@users.noreply.github.com>
2020-11-09 18:55:37 +0200

    Add files via upload

---
+++ suunnittelumallit/strategy_method/StrategyMain.java
@@ -0,0 +1,13 @@
+package strategy_method;
+
+public class StrategyMain {
+
+	public static void main(String[] args) {
+		
+		SortAlgorithms.selectSort();
+		SortAlgorithms.mergeSort();
+		SortAlgorithms.quickSortLoop();
+
+	}
+
+}
